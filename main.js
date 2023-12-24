0989a57dc796609a4f6e924e0399259f5bbe67a4
mey <meydetour@gmail.com>
2023-12-24 18:32:29 +0100

    xx

--- main.js
+++ main.js
@@ -53,6 +53,7 @@ function renderForm() {
 
        </div>`
     render(template)
+    document.querySelector('#loginFormUsername').focus()
     document.querySelector('#loginFormSubmit').addEventListener('click', () => {
         getToken(document.querySelector('#loginFormUsername'), document.querySelector('#loginFormPassword'))
     })
@@ -74,34 +75,35 @@ function renderForm() {
 
 function renderMessages() {
     let allMessages = ''
-    listeMessage.forEach(message => {
-        allMessages += renderMessage(message)
+    if (listeMessage.length !== 0) {
+        listeMessage.forEach(message => {
+            allMessages += renderMessage(message)
+        })
+    }
 
-    })
+
     let fil = `
-
-
          <div class="filDeDiscussion">
                 <div class="banner">
-                <div class="bannerIc">
-                    <i class="bi bi-x-lg"></i>
+                    <div class="bannerIc">
+                        <i class="bi bi-x-lg"></i>
+                    </div>
+                    <h1 class="bannerTitle">Welcome to Nova</h1>
+                    <p>
+                        In this classroom chat, we design, improve, try, try again, try again again and have fun with our
+                        app !
+                        <br>Then if like us you like have some fun, come here and try it !
+                        <br><br>
+                        Made by the best developper Mey 😉
+                    </p>
                 </div>
-                <h1 class="bannerTitle">Welcome to Nova</h1>
-                <p>
-                    In this classroom chat, we design, improve, try, try again, try again again and have fun with our
-                    app !
-                    <br>Then if like us you like have some fun, come here and try it !
-                    <br><br>
-                    Made by the best developper Mey 😉
-                </p>
-            </div>
          
          ${allMessages}
         </div>
         <div class="postMessageContainer">
             <textarea name="postmessage" placeholder="Write text..." id="postmessage" ></textarea>
-            <div class="postmessageBtn"><i class="bi bi-send"></i></div>
-            <div onclick="run()"><i class="bi bi-arrow-clockwise"></i></div>
+            <div class="postmessageBtn"><span>Envoyer</span><i class="bi bi-send"></i></div>
+            <div class="postmessageBtn" onclick="run()"><i class="bi bi-arrow-clockwise"></i></div>
         </div>
     `
     render(fil)
@@ -116,16 +118,19 @@ function renderMessages() {
 }
 
 function renderMessage(message) {
+    let id = message.id
+    let classes = messagePosition(message)
+
     let param = {
-        classe :`message`,
-        container : 'messageContainer',
-        option : `
+        classe: `message ${classes}`,
+        container: 'messageContainer',
+        option: `
                  <div class="messageIc">    
-                   <i  id="${message.id} class="bi bi-chat-left-heart"></i>
-                  <i  id="${message.id} class="bi bi-chat"></i>
+                   <i  id="${id} "class="bi bi-chat-left-heart"></i>
+                   <i  id="${id}" class="bi bi-chat"></i>
                 </div>
     `,
-        dpn : message.author.displayName
+        dpn: message.author.displayName
     }
 
     if (!message.author.displayName) {
@@ -134,84 +139,73 @@ function renderMessage(message) {
 
     if (user.id === message.author.id) {
         param.container = 'messageContainer1',
-        param.option = `
+            param.option = `
                 <div class="messageIc" ">
-                     <i id="${message.id}" class="bi bi-pencil"></i>
-                 <i id="${message.id}" class="bi bi-trash3"></i>
+                     <i id="${id}" class="bi bi-pencil"></i>
+                 <i id="${id}" class="bi bi-trash3"></i>
                 </div>
     `
         param.dpn = 'Vous : ' + param.dpn
-        param.classe = 'messageReverse'
+        param.classe = `messageReverse  ${classes}`
     }
 
     let template = `
-<div class="${param.container}">
- <img src="${user.imageUrl}" alt="Image de profil" class="messageImage">
-    <h2 class="messageAuteur">${param.dpn}</h2> 
-        
-     <div class="${param.classe}" id="message${message.id}">
-                <div class="w-100 h-100">
-                   
-                    <div class="w-100 d-flex flex-row align-items-top">
-                        <textarea readonly class="" name="messageContenu" id="messageContenu" >${message.content}</textarea>
-                        <button type="submit" class="d-none boutonForm editmessageSubmit${message.id}"> Modifier </button>
-                    </div>
-           </div>
-     
-        </div>  
-</div>
+        <div class="${param.container}">
+           <img src="${user.imageUrl}" alt="Image de profil" class="messageImage">
+            <div class="messageAuteurOption">
+                 <span class="messageAuteur">${param.dpn}</span>    
+                  ${param.option}
+            </div> 
+                
+             <div class="${param.classe}" id="message${id}">
+                        <div class="w-100 h-100">
+                           
+                            <div class="w-100 d-flex flex-row align-items-top">
+                                <textarea readonly class="" name="messageContenu" id="messageContenu" >${message.content}</textarea>
+                                <button type="submit" class="d-none boutonForm editmessageSubmit${id}"> Modifier </button>
+                            </div>
+                   </div>
+             
+             </div>  
+            
+         
+        </div>
  
            
 `
     return template
 
-    //   <div class="d-flex flex-column align-items-center justify-content-center gap-2">
-    //              <img src="${user.imageUrl}" alt="Image de profil" class="messageImage">
-    //
-    //        ${param.option}
-    //             </div>
+
 }
 
-function renderInterface(){
+function renderInterface() {
     navbar.innerHTML = `
 
       <div class="navbarListePV">
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-            <button class="btn "><img src="images/defaultimg.png" class="convPvImg" alt=""><h6>contact</h6></button>
-
+            <div class="pvBanner">
+            <span>Chats</span>
+              <div onclick="toggleParam()" class= " centered">
+                    <img src="images/defaultimg.png" alt="" class="navbarParamImage">
+               </div>
+            </div>
+            <button class="pvContact">
+            <img src="images/defaultimg.png" class="convPvImg" alt="">
+            <div class="pvContactData ">
+             <h6>contact</h6>
+              <span>Dernier message envoy2xxxxxxxxxxxxxxxxxxxx</span>
+            </div>
+           
+            </button>
+         
         </div>
-    <div onclick="toggleParam()" class="navbarParamContainer centered">
-        <img src="images/defaultimg.png" alt="" class="navbarParamImage">
-    </div>
+  
     `
 }
 
 function toggleParam() {
-document.querySelector('.paramContainer').classList.toggle('d-none')
+    document.querySelector('.paramContainer').classList.toggle('d-none')
 }
+
 //==============================
 function isEmpty(elt) {
     return elt === ''
@@ -249,14 +243,12 @@ function addEvent() {
             }
         }
     )
-    document.querySelectorAll('.bi-chat-left-heart').forEach(icone=>{
-        icone.addEventListener('click',()=>{
+    document.querySelectorAll('.bi-chat-left-heart').forEach(icone => {
+        icone.addEventListener('click', () => {
             document.querySelector(`.message${icone.id} > div > div> textarea`)
 
         })
     })
-
-
 
 
 }
@@ -270,6 +262,58 @@ function scrollY() {
     document.querySelector('.filDeDiscussion').scrollTo(0, document.querySelector('.filDeDiscussion').scrollHeight)
 }
 
+function messagePosition(message) {
+    let id = message.id
+    'messageStart messageMiddle messageEnd '
+    for (let k = 0; k < listeMessage.length; k++) {
+
+        let id_current_mess = listeMessage[k].author.id
+
+        //Select message in the message list
+        if (id_current_mess === id) {
+            {
+                //is the only message
+                if (k === 0 && k + 1 === listeMessage.length) {
+                    return ' messageMiddle'
+                }
+                //is the first message
+                else if (k === 0) {
+
+                    if (id_current_mess === listeMessage[k + 1].author.id) {
+                        return ' messageStart'
+                    }
+                    return ' messageMiddle'
+                }
+                //is the last message
+                else if (k + 1 === listeMessage.length){
+                    if(listeMessage[k-1].author.id === id_current_mess){
+                        return ' messageEnd'
+                    }
+                    return ' messageMiddle'
+                }
+                else {
+                    //if precedent and next message is yours
+                    if(listeMessage[k-1].author.id === id_current_mess && id_current_mess === listeMessage[k + 1].author.id){
+                        return ' messageMiddle'
+                    }
+                    //if precendent is yours but not the next
+                    else if(listeMessage[k-1].author.id === id_current_mess){
+                        return ' messageEnd'
+                    }
+                    //if next is yours but not the precedent
+                    else if(listeMessage[k+1].author.id === id_current_mess){
+                        return ' messageStart'
+                    }
+                    return ' messageMiddle'
+                }
+
+            }
+        }
+
+
+    }
+
+}
 
 //============================== FETCH
 
@@ -327,21 +371,24 @@ async function getMessage() {
         })
 }
 
-async function sendReact(contenu){
+async function sendReact(contenu) {
     let param = {
-        method:'post',
-        headers:{'Content-type':'application/json',
-        'Authorization':`bearer ${token}`},
+        method: 'post',
+        headers: {
+            'Content-type': 'application/json',
+            'Authorization': `bearer ${token}`
+        },
         body: JSON.stringify({
             content: contenu
         })
     }
-    await fetch(`${baseUrl}api/response/${id}/new`,param)
-        .then(response=>response.json())
-        .then(data =>{
+    await fetch(`${baseUrl}api/response/${id}/new`, param)
+        .then(response => response.json())
+        .then(data => {
             console.log(data)
         })
 }
+
 async function identifier() {
     const param = {
         method: 'GET',
